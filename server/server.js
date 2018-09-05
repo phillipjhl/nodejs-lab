6dac5ae3354526f51a649623e37f5caa657ae282
Phillip <36901887+phillipjhl@users.noreply.github.com>
2018-09-05 17:37:18 -0500

    jk actually completed  part 1

--- server/server.js
+++ server/server.js
@@ -6,6 +6,14 @@ let data = ["chirp one", "chirp two", "chirp three", "chirp four", "chirp five"]
 
 let dataPath = path.join(__dirname, '../chirps.json');
 // console.log(dataPath);
-fs.writeFile(dataPath, JSON.stringify(data), 'utf-8', err => {
-    if(err) console.log(err);
+fs.writeFile(dataPath, JSON.stringify(data), {encoding:'UTF-8'}, err => {
+        if(err) console.log(err);
+});
+
+fs.readFile(dataPath, {encoding: 'UTF-8'}, (err, res) => {
+    if (err) console.log(err);
+    let chirps = JSON.parse(res);
+    chirps.forEach(chirp => {
+        console.log(chirp);
+    });
 });
